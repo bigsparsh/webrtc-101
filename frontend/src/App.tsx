08cd286780a14ca52d4f7819a03c09c995462409
Sparsh Singh <sparshsingh7586@gmail.com>
2024-08-26 12:29:36 +0530

    Fixed some more warnings

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Lobby from "./components/Lobby";
-import Room from "./components/Room";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/lobby" element={<Lobby />} />
-        <Route path="/room" element={<Room />} />
       </Routes>
     </BrowserRouter>
   );
